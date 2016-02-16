bdda1b6711d5ee4a698bda7be07ae8753cfee70c
kennp174 <PhoebeKennedy@Phoebes-Air-2.wireless.newschool.edu>
2016-02-16 17:57:16 -0500

    classwork feb16

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,8 +1,11 @@
 /*
   Classwork
 */
-var ballX = 0;
-var speed = 3;
+var ball1X = 26;
+var ball2Y =26;
+var speed1 = 3;
+var speed2 = 3;
+var color1 = "white"
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -10,12 +13,35 @@ function setup(){
 
 function draw(){
   background("white");
-  ellipse(ballX, height/2, 50,50);
+  fill(color1);
+  ellipse(ball1X, height/2, 50,50);
+  ellipse(width/2, ball2Y, 50,50);
 
-  if (ballX >= width){
-    speed = -3;
+
+
+  ball1X = ball1X + speed1;
+  ball2Y = ball2Y + speed2;
+
+
+  if(ball1X >= windowWidth-25){
+    speed1 = speed1*-1;
+  }
+  if(ball1X <= 25){
+    speed1 = speed1*-1;
+  }
+  if(ball2Y >= windowHeight-25){
+    speed2 = speed2*-1;
+  }
+  if(ball2Y <= 25){
+    speed2 = speed2*-1;
   }
 
-  ballX = ballX + speed;
+  if(mouseIsPressed){
+    color1 = "blue";
+  } else {
+    color1 = "white"
+
+
+  }
 
 }
