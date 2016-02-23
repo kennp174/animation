8a6791aa18d304ce3e3d79ef9edf6374672a6b93
kennp174 <PhoebeKennedy@Phoebes-MacBook-Air-2.local>
2016-02-23 16:52:22 -0500

    black/white vertical/horizontal

--- homework/homework.js
+++ homework/homework.js
@@ -2,14 +2,38 @@
   Homework
 */
 
-//declare your variables
+
+var ball1X = 101;
+var speed1 = 5;
+
+var color1 = "white"
+var color2 = "black"
 
 function setup(){
-  createCanvas(windowWidth,windowHeight);
-
-  //set things up
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-  //draw
+  background("white");
+  fill(color2);
+  ellipse(ball1X, height/2, 200,200);
+
+
+  ball1X = ball1X + speed1;
+
+
+
+  if(ball1X >= windowWidth-100){
+    speed1 = speed1*-1;
+  }
+  if(ball1X <= 100){
+    speed1 = speed1*-1;
+  }
+
+
+  if(mouseIsPressed){
+    background("black");
+    fill(color1);
+    ellipse(width/2, ball1X, 200,200);
+  }
 }
